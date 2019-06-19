0387adcb430a73ce31a9877c9297623640dca327
Ada Skrzypiec <adriannaskrzypiec@o2.pl>
2019-06-19 19:47:01 +0200

    dodanie comboBoxow

--- projekt/Form2.Designer.cs
+++ projekt/Form2.Designer.cs
@@ -38,12 +38,19 @@
             this.Age_txt = new System.Windows.Forms.TextBox();
             this.Save_btn = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(118, 74);
+            this.label1.Location = new System.Drawing.Point(66, 69);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(18, 13);
@@ -53,7 +60,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(118, 119);
+            this.label2.Location = new System.Drawing.Point(66, 114);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 13);
@@ -63,7 +70,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(118, 162);
+            this.label3.Location = new System.Drawing.Point(66, 157);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 13);
@@ -73,7 +80,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(118, 206);
+            this.label4.Location = new System.Drawing.Point(66, 201);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(26, 13);
@@ -82,7 +89,7 @@
             // 
             // Eid_txt
             // 
-            this.Eid_txt.Location = new System.Drawing.Point(208, 72);
+            this.Eid_txt.Location = new System.Drawing.Point(156, 67);
             this.Eid_txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Eid_txt.Name = "Eid_txt";
             this.Eid_txt.Size = new System.Drawing.Size(76, 20);
@@ -90,7 +97,7 @@
             // 
             // Name_txt
             // 
-            this.Name_txt.Location = new System.Drawing.Point(208, 116);
+            this.Name_txt.Location = new System.Drawing.Point(156, 111);
             this.Name_txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name_txt.Name = "Name_txt";
             this.Name_txt.Size = new System.Drawing.Size(76, 20);
@@ -98,7 +105,7 @@
             // 
             // Surname_txt
             // 
-            this.Surname_txt.Location = new System.Drawing.Point(208, 159);
+            this.Surname_txt.Location = new System.Drawing.Point(156, 154);
             this.Surname_txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Surname_txt.Name = "Surname_txt";
             this.Surname_txt.Size = new System.Drawing.Size(76, 20);
@@ -106,7 +113,7 @@
             // 
             // Age_txt
             // 
-            this.Age_txt.Location = new System.Drawing.Point(208, 203);
+            this.Age_txt.Location = new System.Drawing.Point(156, 198);
             this.Age_txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Age_txt.Name = "Age_txt";
             this.Age_txt.Size = new System.Drawing.Size(76, 20);
@@ -114,7 +121,7 @@
             // 
             // Save_btn
             // 
-            this.Save_btn.Location = new System.Drawing.Point(103, 256);
+            this.Save_btn.Location = new System.Drawing.Point(51, 251);
             this.Save_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Save_btn.Name = "Save_btn";
             this.Save_btn.Size = new System.Drawing.Size(75, 23);
@@ -125,19 +132,90 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(209, 256);
+            this.button1.Location = new System.Drawing.Point(51, 292);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 9;
-            this.button1.Text = "Update";
+            this.button1.Text = "Delete";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(156, 251);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(273, 111);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(413, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Choose name";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(273, 66);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(273, 154);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 14;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(413, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Choose ID";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(413, 152);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Choose surname";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 366);
+            this.ClientSize = new System.Drawing.Size(572, 366);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Save_btn);
             this.Controls.Add(this.Age_txt);
@@ -168,5 +246,12 @@
         private System.Windows.Forms.TextBox Age_txt;
         private System.Windows.Forms.Button Save_btn;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
--- projekt/Form2.cs
+++ projekt/Form2.cs
@@ -16,7 +16,88 @@ namespace projekt
         public Form2()
         {
             InitializeComponent();
+            Fillcombo1();
+            Fillcombo2();
+            Fillcombo3();
         }
+
+        void Fillcombo1()
+        {
+            string constring = "datasource=127.0.0.1;port=3306;username=root;password=lksada31";
+            string Query = "select * from database.edata;";
+            MySqlConnection conDataBase = new MySqlConnection(constring);
+            MySqlCommand cmdDataBase = new MySqlCommand(Query, conDataBase);
+            MySqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+                while (myReader.Read())
+                {
+                    string sEid = myReader.GetString("Eid");
+                    comboBox2.Items.Add(sEid);
+                }
+                //funkcja ta pobiera z kolumny "Eid" jej wartosci, zapisuje pod postacia sEid, a nastepnie wyswietla w comboBoxie jako opcje do wyboru
+                conDataBase.Close();
+            }
+       
+            catch (Exception ex) //reprezentuje bledy wystepujace podczas nieprawidlowego polaczenia z BD
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        void Fillcombo2()
+        {
+            string constring = "datasource=127.0.0.1;port=3306;username=root;password=lksada31";
+            string Query = "select * from database.edata;";
+            MySqlConnection conDataBase = new MySqlConnection(constring);
+            MySqlCommand cmdDataBase = new MySqlCommand(Query, conDataBase);
+            MySqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+                while (myReader.Read())
+                {
+                    string sName = myReader.GetString("name");
+                    comboBox1.Items.Add(sName);
+                }
+                conDataBase.Close();
+                //funkcja ta pobiera z kolumny "name" jej wartosci, zapisuje pod postacia sName, a nastepnie wyswietla w comboBoxie jako opcje do wyboru
+            }
+
+            catch (Exception ex) //reprezentuje bledy wystepujace podczas nieprawidlowego polaczenia z BD
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        void Fillcombo3()
+        {
+            string constring = "datasource=127.0.0.1;port=3306;username=root;password=lksada31";
+            string Query = "select * from database.edata;";
+            MySqlConnection conDataBase = new MySqlConnection(constring);
+            MySqlCommand cmdDataBase = new MySqlCommand(Query, conDataBase);
+            MySqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+                while (myReader.Read())
+                {
+                    string sSurname = myReader.GetString("surname");
+                    comboBox3.Items.Add(sSurname);
+                }
+                conDataBase.Close();
+                //funkcja ta pobiera z kolumny name jej wartosci, zapisuje pod postacia sName, a nastepnie wyswietla w comboBoxie jako opcje do wyboru
+            }
+
+            catch (Exception ex) //reprezentuje bledy wystepujace podczas nieprawidlowego polaczenia z BD
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -29,7 +110,7 @@ namespace projekt
             {
                 conDataBase.Open();
                 myReader = cmdDataBase.ExecuteReader();
-                MessageBox.Show("Saved");
+                MessageBox.Show("Saved"); //po prawidlowym dodaniu rekordu wyskoczy okno z napisem Saved
             }
 
             catch (Exception ex) //reprezentuje bledy wystepujace podczas nieprawidlowego polaczenia z BD
@@ -42,6 +123,27 @@ namespace projekt
         private void button1_Click_1(object sender, EventArgs e)
         {
             string constring = "datasource=127.0.0.1;port=3306;username=root;password=lksada31";
+            string Query = "delete from database.edata  where Eid = '" + this.Eid_txt.Text + "' ;";
+            //usunie nam z tabeli rekord o danym nr ID pracownika
+            MySqlConnection conDataBase = new MySqlConnection(constring);
+            MySqlCommand cmdDataBase = new MySqlCommand(Query, conDataBase);
+            MySqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+                MessageBox.Show("Deleted"); //po prawidlowym usunieciu wyskoczy okno z napisem Deleted
+            }
+
+            catch (Exception ex) //reprezentuje bledy wystepujace podczas nieprawidlowego polaczenia z BD
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string constring = "datasource=127.0.0.1;port=3306;username=root;password=lksada31";
             string Query = "update database.edata set Eid = '" + this.Eid_txt.Text + "', name = '" + this.Name_txt.Text + "', surname = '" + this.Surname_txt.Text + "', age = '" + this.Age_txt.Text + "' where Eid = '" + this.Eid_txt.Text + "' ;";
             //dodajemy na koncu where, poniewaz Eid jest kluczem unikalnym i program musi wiedziec do ktorego pracownika odnosi sie zmiana
             MySqlConnection conDataBase = new MySqlConnection(constring);
@@ -51,13 +153,28 @@ namespace projekt
             {
                 conDataBase.Open();
                 myReader = cmdDataBase.ExecuteReader();
-                MessageBox.Show("Updated");
+                MessageBox.Show("Updated"); //po prawidlowym zedytowaniu rekordu wyskoczy okno z napisem Updated
             }
 
             catch (Exception ex) //reprezentuje bledy wystepujace podczas nieprawidlowego polaczenia z BD
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
